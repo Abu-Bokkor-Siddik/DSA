33fb440bd1cfb458b13dd3acccccb92f8049fb81
Md Abu Bokkor Siddik <mdabubokorsiddikbissness@gmail.com>
2025-10-18 22:36:33 +0600

    learn DSA

---
+++ C++/hello.cpp
@@ -0,0 +1,41 @@
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     cout << "Hello, C++ in VS Code!" << endl;
+//     return 0;
+// }
+// #include <iostream>
+// using namespace std;
+// int main()
+// {
+//     cout << "hello siddik" << endl;
+//     return 0;
+// }
+
+// #include <iostream>
+// using namespace std;
+
+// int main()
+// {
+//     int age;
+//     cout << "Enter your age: ";
+//     cin >> age; // cin হলো istream এর অবজেক্ট
+//     cout << "You entered: " << age << endl;
+//     return 0;
+// }
+#include <iostream>
+using std::cout;
+int main()
+{
+    // cout << "inter your name" << std::endl;
+    int n1 = 0;
+    while (n1 <= 10)
+    {
+        cout << "hello while" << std::endl;
+        n1 = n1 + 1;
+    }
+
+    return 0;
+}
